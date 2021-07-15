a24e53111dd0c9e59a8d6c7c3c7fa5c62245dada
InbalEl <inbalelcode@gmail.com>
2021-07-15 10:18:40 +0300

    clean

---
+++ basic-redux/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter: {counterValue}</h1>
+      <button id='increment'>+</button>
+      <button id='decrement'>-</button>
+    </div>
+  );
+}
+
+export default App;
